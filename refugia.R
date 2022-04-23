a18a3779ddb8858e1b5e1d522db3e77737574a71
Lauren Jenkins <lauren.jenkins@my.wheaton.edu>
2022-04-22 19:37:27 -0500

    Update refugia.R

--- refugia.R
+++ refugia.R
@@ -6,6 +6,7 @@ library(xlsx)
 library(geosphere)
 library(raster)
 library(data.table)
+library(dplyr)
 
 setwd('/Volumes/lj_mac_22/MOBOT/PVMvsENM')
 # setwd('/mnt/research/TIMBER/PVMvsENM')
@@ -47,7 +48,7 @@ for(a in 1:length(speciesList)) {
     
     load(paste0(folderName, '/Model ', i, '.Rdata'))
     
-    temp <- enmSdm::thresholdWeighted(predPres, predBg)
+    temp <- enmSdm::thresholdWeighted(predPres, predBg, na.rm = T)
     t <- append(t, temp['msss'])
   }
   thresholds[[a]] <- t
@@ -57,3 +58,54 @@ for(a in 1:length(speciesList)) {
 thresholds <- data.frame(t(rbindlist(thresholds, fill = T)))
 colnames(thresholds) <- speciesList
 rownames(thresholds) <- paste("K-fold", 1:5) 
+
+thresholds <- rbind(thresholds, mean = summarize_all(thresholds, mean))
+
+fileName <- list.files(path = paste0('./predictions/', gcm),
+                       pattern = paste0('PC', pc,'.tif'),
+                       full.names = T)
+threshold <- 0.572
+for(f in fileName) {
+  s <- gsub('\\..*', '', gsub('\\./predictions/*', '', f))
+  speciesAb_ <-  gsub('\\_GCM.*', '', gsub(paste0('\\./predictions/', gcm, '/*'), '', f))
+  load(paste0('./Models/Maxent/model_outputs/', speciesAb_, '_GCM', gcm, 
+              '_PC', pc, '.rData'))
+  pdf(file = paste0('./predictions/PDF_output/', s, '.pdf'), width = 11, height = 8.5)
+  b <- brick(f)
+  names(b) <- c(paste0(seq(21000, 0, by = -1000), ' ybp'))
+  title <- gsub('.*/', '', s)
+  for (i in 1:22) {
+    # par(mfrow=c(1,2))
+    refugia <- b[[i]] >= threshold
+    refugiaId <- raster::clump(refugia, directions = 8, gaps = F)
+    names(refugiaId) <- 'refugiaId'
+    plot(refugiaId, main = paste0(names(b[[i]]),' ', title), axes = F)
+    # plot(b[[22]], main = paste0(names(b[[22]]),' ', title),  col = colors, axes = F)
+    abund <- b[[i]] * refugia
+    names(abund) <- 'refugiaAbund'
+    
+    nrows <- nrow(b[[i]])
+    ncols <- ncol(b[[i]])
+    ncells <- raster::ncell(b[[i]])
+    
+    v <- rep(seq(nrows * (ncols - 1) - 1, 1, by=-ncols), each=ncols) + 0:(ncols - 1)
+    cellNum <- matrix(v, nrow=nrows, ncol=ncols, byrow=TRUE)
+    cellNum <- raster::raster(cellNum, template=b[[i]])
+    cellNum <- as.vector(cellNum)
+    simRefugiaBinary <- as.vector(refugia)
+    refugeCellNum <- cellNum[simRefugiaBinary]
+    if (any(is.na(refugeCellNum))) refugeCellNum <- refugeCellNum[!is.na(refugeCellNum)]
+    
+    # mean refuge abundance
+    meanRefugeAbund <- raster::cellStats(abund, 'sum') / length(refugeCellNum)
+    
+    out <- list(
+      simulationScale = raster::stack(refugiaId, abund),
+      refugeCellNum = refugeCellNum,
+      meanRefugeAbund = meanRefugeAbund
+    )
+  }
+  dev.off()
+}
+
+i <- 1
