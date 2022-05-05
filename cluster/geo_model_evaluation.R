610cbcf42f8d224c272e779e9c583350f0a18cd0
Lauren Jenkins <lauren.jenkins@my.wheaton.edu>
2022-05-05 15:57:06 -0500

    Update geo_model_evaluation.R

--- cluster/geo_model_evaluation.R
+++ cluster/geo_model_evaluation.R
@@ -18,9 +18,14 @@ setwd('/mnt/research/TIMBER/PVMvsENM')
 
 pc <- 5
 predictors <- c(paste0('pca', 1:pc))
-speciesList <- c('Fraxinus americana','Fraxinus caroliniana', 'Fraxinus cuspidata',
-                 'Fraxinus greggii', 'Fraxinus nigra', 'Fraxinus pennsylvanica',
-                 'Fraxinus profunda', 'Fraxinus quadrangulata')
+
+if (gcm == 'Lorenz_ccsm') speciesList <- paste('Fraxinus', c('cuspidata', 'greggii'))
+if (gcm == 'ecbilt') speciesList <- paste('Fraxinus', c('americana', 'cuspidata',
+                                                        'greggii', 'profunca'))
+
+# speciesList <- c('Fraxinus americana','Fraxinus caroliniana', 'Fraxinus cuspidata',
+#                  'Fraxinus greggii', 'Fraxinus nigra', 'Fraxinus pennsylvanica',
+#                  'Fraxinus profunda', 'Fraxinus quadrangulata')
 
 for(s in 1:length(speciesList)) {
   sp <- speciesList[s]
@@ -36,6 +41,8 @@ for(s in 1:length(speciesList)) {
               speciesAb, '.Rdata'))
   load(paste0('./in/models/maxent/all_model_outputs/', speciesAb_, '_GCM', gcm, 
               '_PC', pc, '.Rdata'))
+  load(paste0('./in/workspaces/05 - Modeling Workspace - ', speciesAb_,
+              ' Model Output - PC', pc, '_GCM_', gcm))
   # load(paste0('./regions/little_range_map/', rangeName, '.Rdata'))
   folderName <- paste0('./in/models/maxent/', speciesAb_,
                        '_Maxent/Model Evaluation - Geographic K-Folds - ', gcm)
@@ -48,31 +55,22 @@ for(s in 1:length(speciesList)) {
   # create g-folds
   gPres <- geoFold(x = records, k = 5, minIn = 5, minOut = 10, longLat = c('longitude', 'latitude'))
   
-  isNa <- is.na(rowSums(bgCalib))
-  if (any(isNa)) {
-    bgCalib <- bgCalib[-which(isNa), ]
-  }
-  
-  # only keep 10,000 background sites
-  bgCalib <- bgCalib[1:10000,]
-  randomBg <- bgCalib
-  
   # now, we have our folds, but we want to divide the bg sites into fold based
   # on where they are in relation to records
   
   # initialize vectors to store g-fold assignments
   # gTrainBg <- rep(NA, nrow(randomBg))
-  gTestBg <- rep(NA, nrow(randomBg))
+  gTestBg <- rep(NA, nrow(bg))
   
   # convert records to sp object for gDistance function
   sp.records <- records
   coordinates(sp.records) <- ~longitude+latitude
-  sp.randomBg <- randomBg
+  sp.randomBg <- bg
   coordinates(sp.randomBg) <- ~longitude+latitude
   
   # divide bg sites between training & test
   nearest <- apply(gDistance(sp.records, sp.randomBg, byid = T), 1, which.min)
-  for (k in 1:nrow(randomBg)) {
+  for (k in 1:nrow(bg)) {
     gTestBg[k] <- gPres[nearest[k]]
   }
   
@@ -81,7 +79,7 @@ for(s in 1:length(speciesList)) {
     # for presence/background
     envData <- rbind(
       records[gPres!=m, predictors],
-      randomBg[gTestBg!=m, predictors]
+      bg[gTestBg!=m, predictors]
     )
     
     presBg <- c(rep(1, sum(gPres!=m)), rep(0, sum(gTestBg!=m)))
@@ -89,7 +87,10 @@ for(s in 1:length(speciesList)) {
     trainData <- cbind(presBg, envData)
     
     # Maxent model
-    model <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg')
+    # model <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg')
+    model_tune <- enmSdm::trainMaxNet(data = trainData, resp = 'presBg', 
+                                      out = c('models', 'tuning'))
+    model <- model_tune$models[[96]]
     
     # predict presences & background sites
     predPres <- raster::predict(model, 
@@ -97,12 +98,12 @@ for(s in 1:length(speciesList)) {
                                 clamp = F,
                                 type = 'cloglog')
     predBg <- raster::predict(model, 
-                              newdata = randomBg[gTestBg == m,],
+                              newdata = bg[gTestBg == m,],
                               clamp = F,
                               type = 'cloglog')
     
-    save(model, presBg, predBg, gPres, gTestBg,
-         model, file = paste0(folderName, '/Model ', m, '.Rdata'), compress = T)
+    save(model, predPres, predBg, gPres, gTestBg,
+         model_tune, file = paste0(folderName, '/Model ', m, '.Rdata'), compress = T)
     
     # evaluate
     thisEval <- evaluate(p = as.vector(predPres), a = as.vector(predBg))
@@ -117,39 +118,38 @@ for(s in 1:length(speciesList)) {
   save(aucGeog, cbiGeog, file = paste0(folderName, '/auc_cbi_vals.Rdata'))
 }
 
+speciesList <- c('Fraxinus americana','Fraxinus caroliniana', 'Fraxinus cuspidata',
+                 'Fraxinus greggii', 'Fraxinus nigra', 'Fraxinus pennsylvanica',
+                 'Fraxinus profunda', 'Fraxinus quadrangulata')
+gcmList <- c('Beyer', 'ecbilt', 'Lorenz_ccsm')
+for(gcm in gcmList) {
+  a <- data.frame(c(seq(1:5)))
+  c <- data.frame(c(seq(1:5)))
+  colnames(a)[1] <- colnames(c)[1] <- 'fold #'
+  for(sp in speciesList) {
+    sp <- sp
+    species <- gsub(tolower(sp), pattern=' ', replacement='_')
+    speciesAb <- paste0(substr(sp,1,4), toupper(substr(sp,10,10)), substr(sp,11,13))
+    speciesAb_ <- sub("(.{4})(.*)", "\\1_\\2", speciesAb)
 
-# speciesList <- c('Fraxinus americana','Fraxinus caroliniana', 'Fraxinus cuspidata', 
-#                  'Fraxinus greggii', 'Fraxinus nigra', 'Fraxinus pennsylvanica', 
-#                  'Fraxinus profunda', 'Fraxinus quadrangulata')
-# gcmList <- c('Beyer', 'ecbilt', 'Lorenz_ccsm')
-# for(gcm in gcmList) {
-#   a <- data.frame(c(seq(1:5)))
-#   c <- data.frame(c(seq(1:5)))
-#   colnames(a)[1] <- colnames(c)[1] <- 'fold #'
-#   for(sp in speciesList) {
-#     sp <- sp
-#     species <- gsub(tolower(sp), pattern=' ', replacement='_')
-#     speciesAb <- paste0(substr(sp,1,4), toupper(substr(sp,10,10)), substr(sp,11,13))
-#     speciesAb_ <- sub("(.{4})(.*)", "\\1_\\2", speciesAb)
-#     
-#     folderName <- paste0('./Models/Maxent/', speciesAb_, 
-#                          '_Maxent/Model Evaluation - Geographic K-Folds - ', gcm)
-#     load(paste0(folderName, '/auc_cbi_vals.Rdata'))
-#     
-#     a <- cbind(a, aucGeog)
-#     c <- cbind(c, cbiGeog)
-#     n <- ncol(a)
-#     colnames(a)[n] <- colnames(c)[n] <- sp
-#   }
-#   # save(a, c, file = paste0('./Models/Maxent/', gcm, '_evals.Rdata'))
-#   save(a, c, file = paste0('./Models/Maxent/', gcm, '_geoEvals.Rdata'))
-# }
-# 
-# 
-# for (gcm in gcmList) {
-#   load(paste0('./Models/Maxent/', gcm, '_geoEvals.Rdata'))
-#   write.xlsx(a, file = './Models/Maxent/all_geoEvals.xlsx', sheetName = paste0(gcm, '_auc'),
-#              append = T, row.names = F)
-#   write.xlsx(c, file = './Models/Maxent/all_geoEvals.xlsx', sheetName = paste0(gcm, '_cbi'),
-#              append = T, row.names = F)
-# }
+    folderName <- paste0('./Models/Maxent/', speciesAb_,
+                         '_Maxent/Model Evaluation - Geographic K-Folds - ', gcm)
+    load(paste0(folderName, '/auc_cbi_vals.Rdata'))
+
+    a <- cbind(a, aucGeog)
+    c <- cbind(c, cbiGeog)
+    n <- ncol(a)
+    colnames(a)[n] <- colnames(c)[n] <- sp
+  }
+  # save(a, c, file = paste0('./Models/Maxent/', gcm, '_evals.Rdata'))
+  save(a, c, file = paste0('./Models/Maxent/', gcm, '_geoEvals.Rdata'))
+}
+
+
+for (gcm in gcmList) {
+  load(paste0('./Models/Maxent/', gcm, '_geoEvals.Rdata'))
+  write.xlsx(a, file = './Models/Maxent/geo_evals.xlsx', sheetName = paste0(gcm, '_auc'),
+             append = T, row.names = F)
+  write.xlsx(c, file = './Models/Maxent/geo_evals.xlsx', sheetName = paste0(gcm, '_cbi'),
+             append = T, row.names = F)
+}
